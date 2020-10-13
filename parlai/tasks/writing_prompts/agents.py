bc86605d4b3c523cf273534a8aa364250aefd1f7
David Wilmot <david.wilmot@ed.ac.uk>
2020-10-13 19:09:03 +0100

    Add the writing_prompts dataset task which alternates sentences between the teacher and the agent.

---
+++ parlai/tasks/writing_prompts/agents.py
@@ -0,0 +1,61 @@
+import os
+
+import more_itertools
+from datasets import load_dataset
+
+from parlai.core.teachers import DialogTeacher
+
+_HUGGINGFACE_SCRIPT = os.getenv('WRITING_PROMPTS_DATASET_SCRIPT',
+                                "https://raw.githubusercontent.com/dwlmt/story-datasets/main/WritingPrompts/hugging_face_dataset.py")
+
+
+class WritingPromptsDialogTeacher(DialogTeacher):
+    @staticmethod
+    def add_cmdline_args(parser):
+        parser = parser.add_argument_group('WritingPrompts Teacher Args')
+        parser.add_argument(
+            '--writing-prompts-config-name',
+            type=str,
+            default="writing_prompts_sentence",
+            help="The WritingPrompts huggingface configs name to load.",
+        )
+
+    def __init__(self, opt, shared=None):
+        self.datatype = opt['datatype']
+
+        if opt['datatype'].startswith('train'):
+            suffix = 'train'
+        elif opt['datatype'].startswith('valid'):
+            suffix = 'validation'
+        else:
+            suffix = 'test'
+
+        self.id = 'writing_prompts'
+        self.split = suffix
+        self.config_name = opt["writing_prompts_config_name"]
+
+        opt['datafile'] = os.path.join(os.path.dirname(os.path.realpath(__file__)), _HUGGINGFACE_SCRIPT)
+
+        super().__init__(opt, shared)
+
+    def setup_data(self, path):
+        print('loading: ' + path)
+
+        dataset = load_dataset(path, name=self.config_name, split=self.split)
+        for story in dataset:
+
+            passage_pairs = more_itertools.chunked(story["passages"], n=2)
+            passage_pairs = [p for p in passage_pairs if len(p) > 1 and p[1] is not None]
+            passage_pairs_len = len(passage_pairs)
+            for i, passage_pair in enumerate(passage_pairs):
+                end_of_episode = i + 1 == passage_pairs_len
+                yield {"text": passage_pair[0]["text"], "labels": passage_pair[1]["text"]}, end_of_episode
+
+
+class ExtrasTeacher(WritingPromptsDialogTeacher):
+    def __init__(self, opt, shared=None):
+        super().__init__(opt, shared, extras=True)
+
+
+class DefaultTeacher(WritingPromptsDialogTeacher):
+    pass
